533dab4c54f4cf1b1ee59443f366a4da9800e98a
gurmeet95 <cgurmeet8@gmail.com>
2022-12-25 07:41:36 +0530

    NamasteRect Starting

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading= React.createElement("h1",{id:"title"},"Namaste Everyone! From react");
+    const root=ReactDOM.createRoot(document.getElementById("root"));
+
+
+    // Passing a react element inside root
+    // what is async and dffr
+    root.render(heading);

88e9798f73da4f3bb0609e95bc9e02917620899c
gurmeet95 <cgurmeet8@gmail.com>
2023-01-03 14:56:53 +0530

    Day 2_Understanding Parcel ad its Powers

--- App.js
+++ App.js
@@ -1,9 +1,34 @@
+/* Parcel Powers =>
+ * Created a Server.
+ * HMR - HOt Module Replacement ==Change show directly after refreshing. (USing FileWatcher Algo.)
+ *  File watcher Algorithm(C++).
+ * .parcel-cache is space for Parcel.
+ * Bundling
+ * Minify
+ * cleaning our code.
+ * Dev and Production Build.
+ * Super Fast Build algorithms.
+ * Image Optimization.
+ * Caching while Development.
+ * Compression.
+ * Compatible with older version of Browser.
+ * Https on development. (npx parcel index.html -https)
+ * consistent Hashing Algorithms.
+ * Zero Config Bundler
+ * 
+ *   
+ * 
+ * Transitive dependecies(Will help React to make faster, bundlers,minifying,Image optimization,other packages)
+*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading1= React.createElement("h1",{id:"title"},"Heading1");  //This come from react core library.
 const heading2= React.createElement("h1",{id:"title2"},"Heading2");
 const container=React.createElement(
     "div",
 {
-    id:"container"
+    id:"container",
+    hello:"world"  //This is props(Properties)
 },
 [heading1,heading2]
 )
